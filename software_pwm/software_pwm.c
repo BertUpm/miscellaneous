f9ec419320e3cf4e4f27e2f8c7793dc8d3ac06e0
lxrobotics <cto@lxrobotics.com>
2013-10-22 16:48:15 +0200

    adding demofile for simple interrupt based software pwm

---
+++ software_pwm/software_pwm.c
@@ -0,0 +1,58 @@
+/**
+ * @author Alexander Entinger, MSc / LXRobotics
+ * @brief this module demonstrate how a software pwm could be implemented
+ * @file software_pwm.ino
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// LED Pin = D13 = PB5
+#define LED_DDR		(DDRB)
+#define LED_PORT	(PORTB)
+#define LED			(1<<5)
+
+uint16_t const duration_1 = 5000; // Unit: Timersteps
+uint16_t const duration_2 = 10000;
+
+void init_application() {
+  // set led to output and turn it off
+  LED_PORT &= ~LED;
+  LED_DDR |= LED;
+  // reset timer register
+  TCNT1 = 0x0000;
+  // set OCR1A register
+  OCR1A = duration_1;
+  // enable output compare match interrupt A
+  TIMSK1 |= (1<<OCIE1A);
+  // set precaler = 256 => 1 Timerstep @ 16 MHz = 16 us, 65535 Timersteps (2^16) = 1,048576 s
+  TCCR1B |= (1<<CS12);
+  // globally enable interrupts
+  sei();
+}
+
+int main(void) {
+	
+	init_application();
+	
+	for(;;) {
+
+    }
+}
+
+// interrupt service routine for timer 1 compare match A
+ISR(TIMER1_COMPA_vect) {
+	uint16_t current_OCR1A = OCR1A;
+	static bool led_on = true;
+	if(led_on) {
+		LED_PORT |= LED;
+		OCR1A = current_OCR1A + duration_1;
+	}
+	else {
+		LED_PORT &= ~LED;
+		OCR1A = current_OCR1A + duration_2;
+	}
+	led_on = !led_on; // toogle led on every call of isr
+}
